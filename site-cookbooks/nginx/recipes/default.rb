51f69d48beab179ab08395dac1f21cdd0884f183
gaishimo <gaithimo@gmail.com>
2013-08-31 13:26:52 +0900

    add base_packages, iptables , nginx

--- site-cookbooks/nginx/recipes/default.rb
+++ site-cookbooks/nginx/recipes/default.rb
@@ -10,3 +10,14 @@
 package "nginx" do
   action :install
 end
+
+service "nginx" do
+  action [ :enable, :start]
+end
+
+template "nginx.conf" do
+  path "/etc/nginx/nginx.conf"
+  owner "root"
+  group "root"
+  mode 0644
+end
